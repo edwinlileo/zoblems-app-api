67597bfc37d3698056bcfa643eda841cc4f76bae
Edwin Liléo <edwin.lileo@icloud.com>
2020-05-24 18:07:28 +0200

    Changed command

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^^#w_84#f60inup%ijtq*$s+u1!xroe6amdb_u8uemnr7i9)u7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'api.appclimate.com']
 
 
 # Application definition
